b714e91c79b3ca9a217129da4b2ba9eb3db76de0
Nishith-S-Dev <nishith.s.acharya@gmail.com>
2025-06-09 00:57:51 +0530

    addition of sorting file with the bubble sort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,25 @@
+let arr = [9,8,4,6,7,6,89,21]
+
+function bubbleSort(arr){
+    let n = arr.length
+    for(let i = 0;i<n-1;i++)
+    {
+        let condition = false;
+        for(let j = 0;j<n-i-1;j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                let temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]= temp;
+                condition = true;
+            }
+        }
+        if(condition===false)
+        {
+            break;
+        }
+    }
+    return arr;
+}
+console.log(bubbleSort(arr))
