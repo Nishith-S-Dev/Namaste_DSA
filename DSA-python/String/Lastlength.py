a638173425dd172e8b8b0bb01f597046804de6e0
Nishith-S-Dev <nishith.s.acharya@gmail.com>
2025-06-27 00:30:21 +0530

    this is on the new section which is on String . This is on the to find the last word length of the String

---
+++ DSA-python/String/Lastlength.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)-1
+        while(n>=0):
+            if s[n]==" ":
+                n -= 1
+            else:
+                break
+        count = 0
+        while(n>=0):
+            if s[n]!= " ":
+                count += 1
+                n -= 1
+            else:
+                break
+        return count
+
+        
