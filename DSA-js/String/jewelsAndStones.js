0bebdc62b6fcdf9d04cdd347843eeab91e5b9b1c
Nishith-S-Dev <nishith.s.acharya@gmail.com>
2025-06-27 17:01:52 +0530

    this is on problem which is called jewels and stones  both opt and brute are avail on py and js

---
+++ DSA-js/String/jewelsAndStones.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let count = 0;
+    let jewelss = new Set()
+     for(let i=0;i<jewels.length;i++)
+     {
+        jewelss.add(jewels[i]);
+     }
+     for(let j=0;j<stones.length;j++)
+     {
+        if(jewelss.has(stones[j]))
+        {
+            count++;
+        }
+     }
+     return count
+
+};
